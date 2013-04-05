28983bd68e8982b43ca1f1b461209e97119dc758
mrnda <mrnda@lipstick.(none)>
2013-04-05 13:44:22 +0200

    EventListNode written and compiling

---
+++ uebung1/EventListNode.java
@@ -0,0 +1,18 @@
+public class EventListNode {
+	private IEvent event;
+	private EventListNode next;
+
+	public IEvent getEvent(){
+		return event;
+	}
+	public void setEvent(IEvent event){
+		this.event = event;
+	}
+
+	public EventListNode getNext(){
+		return next;
+	}
+	public void setNext(EventListNode node){
+		next = node;
+	}
+}
