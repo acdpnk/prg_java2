0b503187cbf52d13bc698205c54bb552aa21c2af
mrnda <mrnda@lipstick.(none)>
2013-04-07 21:02:43 +0200

    departure written and compiling

---
+++ uebung1/Departure.java
@@ -0,0 +1,17 @@
+public class Departure implements IEvent {
+	private int execTime;
+
+	public Departure(int execTime){
+		this.execTime=execTime;
+	}
+
+	public void eventExec(BasicSimulator simulator){
+		simulator.entity.setState(simulator.entity.getState()-1);
+		if(simulator.entity.getState() > 0){
+			simulator.events.putAway(new Departure(simulator.getCurrentSimTime() + (int)(Math.random()*6) + 1));
+		}
+	}
+	public int getExecTime(){
+		return execTime;
+	}
+}
