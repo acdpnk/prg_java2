a31f9134e3553e21fc144358e0c8e621bb237537
mrnda <mrnda@lipstick.(none)>
2013-04-08 20:04:09 +0200

    Simulator written and compiling

--- uebung1/Simulator.java
+++ uebung1/Simulator.java
@@ -1,3 +1,35 @@
 public class Simulator extends BasicSimulator {
-	
+
+	private int simulationEnd;
+	private int iterations;
+
+	public Simulator(int end){
+		simulationEnd = end;
+	}
+
+	public int getSimulationEnd(){
+		return simulationEnd;
+	}
+
+	public void init(){
+		setCurrentSimTime(0);
+		setCurrentQueueLength(0);
+		setLastProbeTime(0);
+		setSimulationEntity(new Queue());
+		events = new EventList();
+		events.putAway(new Arrival(0));
+	}
+	public void run(){
+		while(getCurrentSimTime() < getSimulationEnd() && events.hasContent()){
+			iterations++;
+			IEvent event = events.nextEvent();
+			events.removeFirstEvent();
+			setCurrentSimTime(event.getExecTime());
+			refreshQueueLength();
+			event.eventExec(this);
+			System.out.println("Iteration:       " + iterations + "\nSimulation Time: " + getCurrentSimTime() + "\nQueue Length:    " + getCurrentQueueLength() + "\n");
+		}
+		System.out.println("Average Queue Length over whole simulation: " + getCurrentQueueLength()/getCurrentSimTime());
+	}
+
 }
