920ad007c026a2cec142d3b3e80b0873f4677a85
mrnda <mrnda@lipstick.(none)>
2013-05-06 15:36:31 +0200

    done

--- uebung4/GuiSim.java
+++ uebung4/GuiSim.java
@@ -1,5 +1,11 @@
 public class GuiSim {
 	public static void main(String[] args) {
-		gui.Application app = new gui.Application(true);
+		boolean debug = false;
+
+		for (String arg : args) {
+			if(arg.equals("-v")) debug = true;
+		}
+
+		gui.Application app = new gui.Application(debug);
 	}
 }
